d35d20eec8369dfcdcdd8d6e1667b82fd32c2399
Sturgeon <sturgeoj@nala.roche.com>
2019-12-20 15:53:35 -0800

    added grid layout to scss files, attempted grid layout

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -5,20 +5,53 @@ import Button from "../components/button.component";
 import logoWhite from "../assets/img/logo-white.png";
 
 const LandingPage = () => (
-    <header class="header">
-        <div class="header__logo-box">
-            <img src={logoWhite} alt="logo" class="header__logo"/>
-        </div>
+    <div>
+        <header class="header">
+            <div class="header__logo-box">
+                <img src={logoWhite} alt="logo" class="header__logo"/>
+            </div>
 
-        <div class="header__text-box">
-            <h1 class="heading-primary">
-                <span class="heading-primary--main">Outdoors</span>
-                <span class="heading-primary--sub">is where life happens</span>
-            </h1>
+            <div class="header__text-box">
+                <h1 class="heading-primary">
+                    <span class="heading-primary--main">Outdoors</span>
+                    <span class="heading-primary--sub">is where life happens</span>
+                </h1>
 
-            <Button buttonLabel={"Discover what's outside"} hreference={"/"} />
-        </div>
-    </header>
+                <Button buttonLabel={"Discover what's outside"} hreference={"/"} />
+            </div>
+        </header>
+
+        {/* <section class="grid-test">
+            <div class="row">
+                <div class="col-1-of-2">Col 1 of 2</div>
+                <div class="col-1-of-2">Col 2 of 2</div>
+            </div>
+            <div class="row">
+                <div class="col-1-of-3">Col 1 of 3</div>
+                <div class="col-1-of-3">Col 2 of 3</div>
+                <div class="col-1-of-3">Col 3 of 3</div>
+            </div>
+            <div class="row">
+                <div class="col-1-of-3">Col 1 of 3</div>
+                <div class="col-2-of-3">Col 2 of 3</div>
+            </div>
+            <div class="row">
+                <div class="col-1-of-4">Col 1 of 4</div>
+                <div class="col-1-of-4">Col 2 of 4</div>
+                <div class="col-1-of-4">Col 3 of 4</div>
+                <div class="col-1-of-4">Col 4 of 4</div>
+            </div>
+            <div class="row">
+                <div class="col-1-of-4">Col 1 of 4</div>
+                <div class="col-1-of-4">Col 2 of 4</div>
+                <div class="col-2-of-4">Col 2 of 4</div>
+            </div>
+            <div class="row">
+                <div class="col-1-of-4">Col 1 of 4</div>
+                <div class="col-3-of-4">Col 3 of 4</div>
+            </div>
+        </section> */}
+    </div>
 )
 
 export default LandingPage;
