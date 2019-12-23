9da7229a87c3a072747ab726410fdc1bfa23daac
Sturgeon <sturgeoj@nala.roche.com>
2019-12-23 13:35:02 -0800

    added booking form

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -250,6 +250,29 @@ const LandingPage = () => (
                     <a href="/" className="btn btn--green">See all stories</a>
                 </div>
             </section>
+            <section className="section-book">
+                <div className="row">
+                    <div className="book">
+                        <div className="book__form">
+                            <form action="/" className="form">
+                                <div className="u-margin-bottom-medium">
+                                    <h2 className="heading-secondary">
+                                        Start booking now
+                                    </h2>
+                                </div>
+                                <div className="form__group">
+                                    <input type="text" className="form__input" id="name" placeholder="Full Name" required/>
+                                    <label htmlFor="name" className="form__label">Full Name</label>
+                                </div>
+                                <div className="form__group">
+                                    <input type="email" className="form__input" id="email" placeholder="Email Address" required/>
+                                    <label htmlFor="email" className="form__label">Email Address</label>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     </div>
 )
