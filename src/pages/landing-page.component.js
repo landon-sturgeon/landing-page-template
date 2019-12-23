95d27547339bea7a15666d0a70c2edbf63e1c892
Sturgeon <sturgeoj@nala.roche.com>
2019-12-23 08:45:46 -0800

    added tours details and buttons to choose

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -116,67 +116,100 @@ const LandingPage = () => (
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card__side card__side--front card__side--front--1">
-                                card 1 side 1
+                                <div className="card__picture card__picture--1">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--1">the sea explorer</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>3 day tours</li>
+                                        <li>up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>sleep in cozy hotels</li>
+                                        <li>difficulty: easy</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div className="card__side card__side--back card__side--back--1">
-                                card 1 side 2
+                                <div className="card__cta">
+                                    <div className="card__price--box">
+                                        <p className="card__price--only">Only</p>
+                                        <p className="card__price--value">$297</p>
+                                    </div>
+                                    <a href="/" className="btn btn--white">book now</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card__side card__side--front card__side--front--2">
-                                card 2 side 1
+                                <div className="card__picture card__picture--2">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--2">the land explorer</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>7 day tours</li>
+                                        <li>up to 40 people</li>
+                                        <li>6 tour guides</li>
+                                        <li>sleep in provided tents</li>
+                                        <li>difficulty: medium</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div className="card__side card__side--back card__side--back--2">
-                                card 2 side 2
+                                <div className="card__cta">
+                                    <div className="card__price--box">
+                                        <p className="card__price--only">Only</p>
+                                        <p className="card__price--value">$497</p>
+                                    </div>
+                                    <a href="/" className="btn btn--white">book now</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card__side card__side--front card__side--front--3">
-                                card 3 side 1
+                                <div className="card__picture card__picture--3">
+                                    &nbsp;
+                                </div>
+                                <h4 className="card__heading">
+                                    <span className="card__heading-span card__heading-span--3">the snow explorer</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>5 day tours</li>
+                                        <li>up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>sleep in provided tents</li>
+                                        <li>difficulty: hard</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div className="card__side card__side--back card__side--back--3">
-                                card 3 side 2
+                                <div className="card__cta">
+                                    <div className="card__price--box">
+                                        <p className="card__price--only">Only</p>
+                                        <p className="card__price--value">$897</p>
+                                    </div>
+                                    <a href="/" className="btn btn--white">book now</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div className="u-center-text u-margin-top-huge">
+                    <a href="/" className="btn btn--green">discover all tours</a>
+                </div>
             </section>
         </main>
-
-        {/* <section class="grid-test">
-            <div class="row">
-                <div class="col-1-of-2">Col 1 of 2</div>
-                <div class="col-1-of-2">Col 2 of 2</div>
-            </div>
-            <div class="row">
-                <div class="col-1-of-3">Col 1 of 3</div>
-                <div class="col-1-of-3">Col 2 of 3</div>
-                <div class="col-1-of-3">Col 3 of 3</div>
-            </div>
-            <div class="row">
-                <div class="col-1-of-3">Col 1 of 3</div>
-                <div class="col-2-of-3">Col 2 of 3</div>
-            </div>
-            <div class="row">
-                <div class="col-1-of-4">Col 1 of 4</div>
-                <div class="col-1-of-4">Col 2 of 4</div>
-                <div class="col-1-of-4">Col 3 of 4</div>
-                <div class="col-1-of-4">Col 4 of 4</div>
-            </div>
-            <div class="row">
-                <div class="col-1-of-4">Col 1 of 4</div>
-                <div class="col-1-of-4">Col 2 of 4</div>
-                <div class="col-2-of-4">Col 2 of 4</div>
-            </div>
-            <div class="row">
-                <div class="col-1-of-4">Col 1 of 4</div>
-                <div class="col-3-of-4">Col 3 of 4</div>
-            </div>
-        </section> */}
     </div>
 )
 
