41a1b61748a4d85f63bc554e135c85be86b49cee
Sturgeon <sturgeoj@nala.roche.com>
2019-12-24 10:32:43 -0800

    added rough draft of footer

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -8,6 +8,7 @@ import nat2 from "../assets/img/nat-2-large.jpg";
 import nat3 from "../assets/img/nat-3-large.jpg";
 import nat8 from "../assets/img/nat-8.jpg";
 import nat9 from "../assets/img/nat-9.jpg";
+import logoGreen from "../assets/img/logo-green-2x.png";
 
 const LandingPage = () => (
     <div>
@@ -297,6 +298,30 @@ const LandingPage = () => (
                     </div>
                 </div>
             </section>
+            <footer className="footer">
+                <div className="footer__logo-box">
+                    <img src={logoGreen} alt="Full logo" className="footer__logo"/>
+                </div>
+                <div className="row">
+                    <div className="col-1-of-2">
+                        <div className="footer__navigation">
+                            <ul className="footer__list">
+                                <li className="footer__item"><a href="/" className="footer__link">Company</a></li>
+                                <li className="footer__item"><a href="/" className="footer__link">Contact us</a></li>
+                                <li className="footer__item"><a href="/" className="footer__link">Careers</a></li>
+                                <li className="footer__item"><a href="/" className="footer__link">Privacy Policy</a></li>
+                                <li className="footer__item"><a href="/" className="footer__link">Terms</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-1-of-2">
+                        <p className="footer__copyright">
+                            Built by <a href="/" className="footer__link">Landon Sturgeon</a> whose github can be <a href="/" className="footer__link">found here</a>.
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </main>
     </div>
 )
