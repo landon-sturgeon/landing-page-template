e2d7308b2506d727204a868554d1105f5d8258d5
Sturgeon <sturgeoj@nala.roche.com>
2019-12-24 09:57:03 -0800

    added input formatting and button

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -260,16 +260,40 @@ const LandingPage = () => (
                                         Start booking now
                                     </h2>
                                 </div>
+
                                 <div className="form__group">
                                     <input type="text" className="form__input" id="name" placeholder="Full Name" required/>
                                     <label htmlFor="name" className="form__label">Full Name</label>
                                 </div>
+
                                 <div className="form__group">
                                     <input type="email" className="form__input" id="email" placeholder="Email Address" required/>
                                     <label htmlFor="email" className="form__label">Email Address</label>
                                 </div>
+
+                                <div className="form__group u-margin-bottom-medium">
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="small" name="size"/>
+                                        <label htmlFor="small" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Small tour group</label>
+                                    </div>
+
+                                    <div className="form__radio-group">
+                                        <input type="radio" className="form__radio-input" id="large" name="size"/>
+                                        <label htmlFor="large" className="form__radio-label">
+                                            <span className="form__radio-button"></span>
+                                            Large tour group
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div className="form__group">
+                                    <button className="btn btn--green">Next Step &rarr;</button>
+                                </div>
                             </form>
                         </div>
+
                     </div>
                 </div>
             </section>
