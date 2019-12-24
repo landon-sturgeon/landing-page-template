5a73cf67e5b704a6b002874989d09b7bcf9a267f
Sturgeon <sturgeoj@nala.roche.com>
2019-12-24 12:39:13 -0800

    added animation to navigation button

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -15,7 +15,9 @@ const LandingPage = () => (
         <div className="navigation">
             <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
 
-            <label htmlFor="navi-toggle" className="navigation__button">MENU</label>
+            <label htmlFor="navi-toggle" className="navigation__button">
+                <span className="navigation__icon">&nbsp;</span>
+            </label>
 
             <div className="navigation__background">&nbsp;</div>
 
