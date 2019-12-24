d7d50d8a84afaf1464b47354c526860e492609a5
Sturgeon <sturgeoj@nala.roche.com>
2019-12-24 11:10:55 -0800

    added line items for navigation

--- src/pages/landing-page.component.js
+++ src/pages/landing-page.component.js
@@ -12,6 +12,24 @@ import logoGreen from "../assets/img/logo-green-2x.png";
 
 const LandingPage = () => (
     <div>
+        <div className="navigation">
+            <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
+
+            <label htmlFor="navi-toggle" className="navigation__button">MENU</label>
+
+            <div className="navigation__background">&nbsp;</div>
+
+            <nav className="navigation__nav">
+                <ul className="navigation__list">
+                    <li className="navigation__item"><a href="/" className="navigation__link"><span>01</span>About Website</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link"><span>02</span>Your Benefits</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link"><span>03</span>Popular Tours</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link"><span>04</span>Stories</a></li>
+                    <li className="navigation__item"><a href="/" className="navigation__link"><span>05</span>Book Now</a></li>
+                </ul>
+            </nav>
+        </div>
+
         <header class="header">
             <div class="header__logo-box">
                 <img src={logoWhite} alt="logo" class="header__logo"/>
